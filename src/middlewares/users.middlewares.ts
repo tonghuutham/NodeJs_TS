1d13885f3e353ed100746f094570867dc39b8eb3
tonghuutham <tonghuutham@gmail.com>
2023-08-23 15:27:23 +0700

    Khai báo message validation

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import { checkSchema } from 'express-validator'
+import USE_MESSAGE from '~/constants/messages'
 import { ErrorWithStatus } from '~/models/Errors'
 import databaseService from '~/services/databases.services'
 import usersService from '~/services/users.services'
@@ -19,35 +20,46 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
 export const registerValidator = validate(
   checkSchema({
     name: {
-      notEmpty: true,
+      notEmpty: {
+        errorMessage: USE_MESSAGE.NAME_IS_REQUIRED
+      },
       isLength: {
         options: {
           min: 1,
           max: 100
-        }
+        },
+        errorMessage: USE_MESSAGE.NAME_LENGTH_MUST_BE_FROM_1_TO_100
       },
-      isString: true
+      isString: {
+        errorMessage: USE_MESSAGE.NAME_MUST_BE_A_STRING
+      },
+      trim: true
     },
     email: {
       isEmail: true,
-      notEmpty: true,
+      notEmpty: {
+        errorMessage: USE_MESSAGE.EMAIL_IS_REQUIRED
+      },
       custom: {
         options: async (value) => {
           const isExitsEmail = await usersService.checkMailExit(value) // check email đã có trong db chưa
           if (isExitsEmail) {
-            throw new ErrorWithStatus({ message: 'Email already exists', status: 400 })
+            throw new ErrorWithStatus({ message: USE_MESSAGE.EMAIL_ALREADY_EXISTS, status: 400 })
           }
           return true
         }
       }
     },
     password: {
-      notEmpty: true,
+      notEmpty: {
+        errorMessage: USE_MESSAGE.PASSWORD_IS_REQUIRED
+      },
       isLength: {
         options: {
           min: 6,
           max: 255
-        }
+        },
+        errorMessage: USE_MESSAGE.CONFIRM_PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
       },
       isString: true,
       isStrongPassword: {
@@ -62,12 +74,13 @@ export const registerValidator = validate(
       }
     },
     confirm_password: {
-      notEmpty: true,
+      notEmpty: { errorMessage: USE_MESSAGE.CONFIRM_PASSWORD_IS_REQUIRED },
       isLength: {
         options: {
           min: 6,
           max: 255
-        }
+        },
+        errorMessage: USE_MESSAGE.CONFIRM_PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
       },
       isString: true,
       isStrongPassword: {
@@ -87,6 +100,15 @@ export const registerValidator = validate(
           }
           return true
         }
+      }
+    },
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        },
+        errorMessage: USE_MESSAGE.DATE_OF_BIRTH_MUST_BE_ISO8601
       }
     }
   })
